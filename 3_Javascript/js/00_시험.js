d1e1293ff298c00f7d8e87d964786099c3593158
neosiureu <neosiureu@gmail.com>
2025-04-16 22:48:06 +0900

    html css js

--- 3_Javascript/js/00_시험.js
+++ 3_Javascript/js/00_시험.js
@@ -1,17 +1,32 @@
-const box = document.querySelector(".box");
-
-
-
-
 
 document.querySelector('#changeButton')
 .addEventListener(
 "click",function(){
-    boxList.style.backgroundColor = inputList.value;
-    boxList.style.color = textList.value;
-    boxList.innerHTML = text.value;
-    boxList.style.height= height.value;
-    boxList.style.width= width.value;
+    const box = document.querySelector(".box");
+    // const textList = document.querySelector(".textList");
+    const textColor = document.querySelector(".text-color");
+    const height = document.querySelector(".height-input");
+    const width = document.querySelector(".width-input");
+    const backgroundColorInput = document.querySelector(".backgound-color-input");
+    const textOrigin = document.querySelector(".text-origin");
+    const bold = document.querySelector("[name='bold']:checked")
+    const garo = document.querySelector("[name='garo']:checked")
+    const sero = document.querySelector("[name='sero']:checked")
+    const size= document.querySelector(".size")
+
+
+
+
+
+    box.style.backgroundColor = backgroundColorInput.value;
+    box.style.color = textColor.value;
+    box.style.height= height.value +"px";
+    box.style.width= width.value +"px";
+    box.innerHTML=textOrigin.value;
+    box.style.fontWeight = bold.value;
+    box.style.justifyContent = garo.value;
+    box.style.alignItems=sero.value;
+    box.style.fontSize=size.value+"px";
 
 }
 )
